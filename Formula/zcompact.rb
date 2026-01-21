c931841242d76e96f3cab154191e8776d4e8daa1
zzmarks26 <zzmarks26@gmail.com>
2026-01-20 19:51:31 -0600

    Update to v1.0.1 with system deps support

--- Formula/zcompact.rb
+++ Formula/zcompact.rb
@@ -1,18 +1,22 @@
 class Zcompact < Formula
   desc "JSON compaction with queryable ID index"
   homepage "https://github.com/zzmarks26/Zcompact"
-  url "https://github.com/zzmarks26/Zcompact/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "3d361fa2573e1ec8b25e34e62be2c8261b5d325164f342f364acae7182a1fe2c"
+  url "https://github.com/zzmarks26/Zcompact/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "954583cb0e1e28396097c47e3c7abc311aabb9dd3b977df70a68a3deeaaadd0f"
   license "MIT"
   head "https://github.com/zzmarks26/Zcompact.git", branch: "main"
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "cli11"
+  depends_on "nlohmann-json"
   depends_on "zstd"
   depends_on "zlib"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build",
+           "-DZCOMPACT_USE_SYSTEM_DEPS=ON",
+           *std_cmake_args
     system "cmake", "--build", "build"
 
     bin.install "build/zcompact"
